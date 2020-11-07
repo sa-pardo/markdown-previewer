cb0c45dad30aa03577a50d0e0226d0020f52266e
Sebastian Pardo <sapardosuazo@gmail.com>
2020-11-07 00:13:08 -0300

    Adds onChange logic for textarea

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Editor from './Editor'
 import Previewer from './Previewer'
 
 function App() {
+  const [input, setInput] = useState(initialState)
+
+  const changeHandler = (e) => {
+    const input = e.target.value;
+    setInput(input);
+  }
+
   return (
     <div className="App">
-      <Editor input={initialState}/>
-      <Previewer input={initialState}/>
+      <Editor input={input} onChange={changeHandler}/>
+      <Previewer input={input}/>
     </div>
   );
 }
@@ -17,6 +24,7 @@ const initialState = `
 # H1
 ## H2
 [link](http://sa-pardo.github.io/blog)
+
 \`in line code\`
 \`\`\`javascript
 const welcome = () => console.log("Hello World!");
@@ -26,6 +34,8 @@ const welcome = () => console.log("Hello World!");
 * item 3
 
 > Block quote
+
 ![React Logo w/ Text](https://avatars3.githubusercontent.com/u/43013769?s=460&u=89331c4406d643fd9960e54a5d39ee7d7ed23909&v=4)
+
 **Bolded text**
 `;
--- src/Editor.js
+++ src/Editor.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Editor(props) {
   return (
     <div id="editor-container">
-      <textarea id="editor" defaultValue={props.input}>
+      <textarea id="editor" defaultValue={props.input} onChange={props.onChange}>
       </textarea>
     </div>
   )
