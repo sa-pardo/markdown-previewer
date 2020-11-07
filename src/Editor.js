4e6e2e5c54228181d0574182603fc5519cbee82c
Sebastian Pardo <sapardosuazo@gmail.com>
2020-11-07 13:38:33 -0300

    add header for preview and editor

--- src/Editor.js
+++ src/Editor.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Editor(props) {
   return (
     <div id="editor-container">
+      <div id="header">Editor</div>
       <textarea id="editor" defaultValue={props.input} onChange={props.onChange}>
       </textarea>
     </div>
--- src/Previewer.js
+++ src/Previewer.js
@@ -9,6 +9,7 @@ export default function Previewer(props) {;
   const parsedInput = marked(props.input);
   return (
     <div id="previewer-container">
+      <div id="header">Preview</div>
       <div id="preview" dangerouslySetInnerHTML={{__html: parsedInput}}>
       </div>
     </div>
