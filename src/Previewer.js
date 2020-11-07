a0d5b0df7d675517d15025ab64966bb4aee92386
Sebastian Pardo <sapardosuazo@gmail.com>
2020-11-07 00:13:22 -0300

    adds marked

--- src/Previewer.js
+++ src/Previewer.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import marked from 'marked'
 
+marked.setOptions({
+  breaks: true
+})
+
 export default function Previewer(props) {;
   const parsedInput = marked(props.input);
   return (
     <div id="previewer-container">
-      <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
       <div id="preview" dangerouslySetInnerHTML={{__html: parsedInput}}>
       </div>
     </div>
